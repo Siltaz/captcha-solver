fb20403450b4b7eb2231fb62234b81586e02d1e5
Ritesh Kumar Yadav <siltaz@pm.me>
2021-12-28 11:52:42 +0530

    worker.terminate() removed due to bug

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import { createWorker } from 'tesseract.js';
-import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
 import hpp from 'hpp';
@@ -12,7 +11,6 @@ const worker = createWorker();
 const env = process.env.NODE_ENV;
 const port = process.env.APP_PORT;
 
-app.use(morgan('combined', { skip: (_, res) => (res.statusCode < 400) }));
 app.use(cors());
 app.use(hpp());
 app.use(helmet());
@@ -21,17 +19,20 @@ app.use('/solve_captcha', async (req, res) => {
 
     try {
         const imageUrl = req.query.uri;
+        console.log(`Solving: ${imageUrl}`);
 
         await worker.load();
         await worker.loadLanguage('eng');
         await worker.initialize('eng');
         const { data: { text } } = await worker.recognize(imageUrl);
         const result = eval(text.split("=")[0]);
-        await worker.terminate();
+        // await worker.terminate(); // Causes Issue - need help
 
+        console.log(`Solved: ${result}`);
         res.status(200).json({ result });
     } catch (error) {
-        res.status(500).json({ error })
+        console.log(`Failed: ${error.message}`);
+        res.status(500).json({ error: error.message });
     }
 });
 
