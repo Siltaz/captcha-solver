890c197fe617a6d2586193be0fd49a90240e232c
Ritesh Kumar Yadav <siltaz@proton.me>
2023-07-06 11:31:54 +0530

    tesseract upgrade

--- main.js
+++ main.js
@@ -6,40 +6,43 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 
 const app = express();
-const worker = createWorker();
+const worker = await createWorker();
 
-const env = process.env.NODE_ENV;
-const port = process.env.APP_PORT;
+const env = process.env.NODE_ENV || 'development';
+const port = process.env.APP_PORT || 3000;
 
 app.use(cors());
 app.use(hpp());
 app.use(helmet());
 
 app.use('/solve_captcha', async (req, res) => {
+  try {
+    const imageUrl = req.query.uri;
+    console.log(`Solving: ${imageUrl}`);
 
-    try {
-        const imageUrl = req.query.uri;
-        console.log(`Solving: ${imageUrl}`);
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
 
-        await worker.load();
-        await worker.loadLanguage('eng');
-        await worker.initialize('eng');
-        const { data: { text } } = await worker.recognize(imageUrl);
-        console.log(`OCR Result: ${text}`);
-        const result = eval(text.split("=")[0]);
+    const {
+      data: { text },
+    } = await worker.recognize(imageUrl);
+    console.log(`OCR Result: ${text}`);
 
-        console.log(`Solved: ${result}`);
-        res.status(200).json({ result });
-    } catch (error) {
-        console.log(`Failed: ${error.message}`);
-        res.status(500).json({ error: error.message });
-    }
+    // If it's a mathematical captcha
+    const result = text.includes('=') ? eval(text.split('=')[0]) : text.trim();
+
+    console.log(`Solved: ${result}`);
+    res.status(200).json({ result });
+  } catch (error) {
+    console.log(`Failed: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.use((req, res) => res.status(404).json({ message: 'Not Found' }));
 
 app.listen(port, () => {
-    console.info(`========== ENV: ${env} ==========`);
-    console.info(`🚀 Service listening on the port ${port}`);
-    console.info(`=====================================`);
+  console.info(`========== ENV: ${env} ==========`);
+  console.info(`🚀 Service listening on the port ${port}`);
+  console.info(`=====================================`);
 });
